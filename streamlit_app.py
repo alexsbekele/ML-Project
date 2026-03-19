6099170f233039cb226be8641aa7a0551554157f
Merga Bekele <bmerga52@gmail.com>
2026-03-19 21:33:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Machine Learning App')
 st.info('This is an app for machine learning model')
 
 with st.expander('Data'):
-  st.write('**Raw Data')
+  st.write('**Raw Data**')
 
   df = pd.read_csv('https://raw.githubusercontent.com/alexsbekele/data/main/penguins_cleaned%20(1).csv')
   df
