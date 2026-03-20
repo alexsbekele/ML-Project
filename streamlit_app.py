4f55873630b4549ce670bafa5468f15cf53cc741
Merga Bekele <bmerga52@gmail.com>
2026-03-20 10:10:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,8 @@ with st.expander('Data Visualization'):
     }
   input_df = pd.DataFrame(data, index = [0])
   input_penguins = pd.concat([input_df, x], axis = 0)
-input_df
-#input_penguins
+with st.expander('Input Features'):
+  st.write('**Input Penguin**')
+  input_df
+  st.write('**Compined Penguin data**')
+  input_penguins
