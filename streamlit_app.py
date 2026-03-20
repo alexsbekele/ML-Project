cefafb648f4c2f611b9bc5196a123aea8558343c
Merga Bekele <bmerga52@gmail.com>
2026-03-20 09:35:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,11 @@ with st.expander('Data'):
   Y
 with st.expander('Data Visualization'):
   st.scatter_chart(data = df, x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
+
+  
+  #Data Preparation
+  with st.siderbar:
+    st.header('Input Features')
+    island = st.selectbox('Island',('Biscoe', 'Dream', 'Torgersen') )
+    gender = st.selectbox('Gender', ('male', 'female'))
+    bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
